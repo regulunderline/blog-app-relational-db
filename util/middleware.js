d47346f7c2f5d72f4a39df7a843e6d0a34d0c10a
regul <regulunderline@gmail.com>
2025-11-18 17:57:33 +0300

    13.10

--- util/middleware.js
+++ util/middleware.js
@@ -1,3 +1,8 @@
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('./config')
+
+const { Blog } = require('../models')
+
 const errorHandler = (error, req, res, next) => {
   try {
     error.errors.map(e => {
@@ -11,4 +16,31 @@ const errorHandler = (error, req, res, next) => {
   next(error)
 }
 
-module.exports = { errorHandler }
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (e) {
+      req.decodedToken = {e, error: 'token invalid' }
+    }
+  }  else {
+    req.decodedToken = { error: 'token missing' }
+  }
+  next()
+}
+
+const blogsLinker = async (req, res, next) => {
+  (req.decodedToken && !req.decodedToken.error) && await Blog.update(
+    { userId: req.decodedToken.id },
+    {
+      where: {
+        //userId: null
+      }
+    }
+  )
+
+  next()
+} 
+
+module.exports = { errorHandler, tokenExtractor, blogsLinker }
