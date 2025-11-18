d0e5f7170a34be768bbb13bbf04e4f591267f4ab
regul <regulunderline@gmail.com>
2025-11-18 18:32:51 +0300

    13.12

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
 
 const { errorHandler } = require('../util/middleware')
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
+  const blogs = await Blog.findAll({
+    include: {
+      model: User
+    }
+  })
   res.json(blogs)
 })
 
