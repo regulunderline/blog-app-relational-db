265dc720b036fc9a8bc12a075cd1520f7723f79a
regul <regulunderline@gmail.com>
2025-11-21 18:38:37 +0300

    13.19

--- models/index.js
+++ models/index.js
@@ -1,9 +1,13 @@
 const Blog = require('./blog')
 const User = require('./user')
+const UserBlog = require('./user_blogs')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
+User.belongsToMany(Blog, { through: UserBlog, as: 'marked_blogs' })
+Blog.belongsToMany(User, { through: UserBlog, as: 'blogs_marked' })
+
 module.exports = {
-  Blog, User
+  Blog, User, UserBlog
 }
